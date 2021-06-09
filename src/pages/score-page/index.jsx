0872ac3cbbd3f5b311d1c58376f772d423c94fb3
Pedro Pacheco <pacheckdev@gmail.com>
2021-06-09 17:25:40 -0300

    score page update

--- src/pages/score-page/index.jsx
+++ src/pages/score-page/index.jsx
@@ -2,7 +2,36 @@ import React, { useContext } from "react";
 
 import { MyContext } from "../../container/App";
 
+import {
+  Box,
+  Container,
+  ListItemText,
+  Divider,
+  Typography,
+} from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles";
+
 import { TextHelper } from "../../helper/question-page.helper";
+
+import Report from "../../components/ReportStatus";
+
+const useStyles = makeStyles({
+  box: {
+    height: "100vh",
+    backgroundColor: "#eee",
+    display: "flex",
+  },
+  answer: {
+    backgroundcolor: "black",
+  },
+  wrong: {
+    color: "#b81717",
+  },
+  right: {
+    color: "#117411",
+  },
+});
 
 const ScorePage = () => {
   const {
@@ -16,12 +45,50 @@ const ScorePage = () => {
     },
   } = useContext(MyContext);
 
+  const classes = useStyles();
+
   return (
-    <div className="score-page">
-      <h1>{userName}</h1>
-      <h2>você teve um total de {correctAnswersCounter} acertos</h2>
-      <h2>você teve um total de {wrongAnswersCounter} erros</h2>
-    </div>
+    <Box className={classes.box}>
+      <Container>
+        <Box className={classes.status}>
+          <Typography variant="h2" gutterBottom>
+            {userName},
+          </Typography>
+          <Report
+            correctAnswersCount={correctAnswersCounter}
+            wrongAnswersCount={wrongAnswersCounter}
+          />
+        </Box>
+        <Box className={classes.list}>
+          {questions.map((question) => (
+            <Box key={question.question}>
+              <Typography variant="h4">
+                {TextHelper(question.question)}
+              </Typography>
+              <ul>
+                <Typography
+                  variant="subtitle1"
+                  className={(classes.answer, classes.right)}
+                >
+                  {TextHelper(question.correct_answer)}
+                </Typography>
+                {question.incorrect_answers.map((answer) => (
+                  <>
+                    <Divider />
+                    <Typography
+                      variant="subtitle2"
+                      className={(classes.answer, classes.wrong)}
+                    >
+                      {TextHelper(answer)}
+                    </Typography>
+                  </>
+                ))}
+              </ul>
+            </Box>
+          ))}
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
